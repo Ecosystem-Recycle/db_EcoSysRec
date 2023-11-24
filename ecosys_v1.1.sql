985db56b0eeeaca58bc3449edb2648689e7471f8
Alcides Paz <alcidesnp@hotmail.com>
2023-11-23 23:21:17 -0300

    Corrigido identificadores e alterado atributos para validação

--- ecosys_v1.1.sql
+++ ecosys_v1.1.sql
@@ -1,101 +1,92 @@
 CREATE DATABASE db_ecosystem;
 -- DROP DATABASE db_ecosystem;
-
 USE db_ecosystem;
 
-CREATE TABLE tipoUsuario(
-Id BINARY(16) NOT NULL,
-Nome VARCHAR(255) NOT NULL,
-PRIMARY KEY (Id)
-);
+-- Desativado para testes com uso via chave estrangeira
+-- CREATE TABLE tipo_usuario(
+-- id BINARY(16) NOT NULL,
+-- nome VARCHAR(255) NOT NULL,
+-- PRIMARY KEY (id)
+-- );
 
 CREATE TABLE endereco(
-Id BINARY (16) NOT NULL, 
-Logradouro VARCHAR (255) NOT NULL, 
-Numero VARCHAR (20) NOT NULL,
-Bairro VARCHAR (255) NOT NULL,
-Cidade VARCHAR (255) NOT NULL,
-Estado VARCHAR (255) NOT NULL,
-Cep VARCHAR (20) NOT NULL,
-DataCriacao DATETIME,
-DataModificacao DATETIME,
-PRIMARY KEY (Id)
+id BINARY (16) NOT NULL, 
+logradouro VARCHAR (255) NOT NULL, 
+numero VARCHAR (20) NOT NULL,
+bairro VARCHAR (255) NOT NULL,
+cidade VARCHAR (255) NOT NULL,
+estado VARCHAR (255) NOT NULL,
+cep VARCHAR (20) NOT NULL,
+PRIMARY KEY (id)
 );
 
 CREATE TABLE categoria(
-Id BINARY (16) NOT NULL,
-Nome VARCHAR (255) NOT NULL,
-PRIMARY KEY (Id)
+id BINARY (16) NOT NULL,
+nome VARCHAR (255) NOT NULL,
+PRIMARY KEY (id)
 );
 
-CREATE TABLE tipoStatus(
-	Id BINARY (16) NOT NULL,
-    Nome VARCHAR (255) NOT NULL,
-    PRIMARY KEY (Id)
+CREATE TABLE tipo_status(
+id BINARY (16) NOT NULL,
+nome VARCHAR (255) NOT NULL,
+PRIMARY KEY (id)
 );
 
 
 CREATE TABLE usuario(
-Id BINARY(16) NOT NULL,
-Nome VARCHAR(255) NOT NULL,
-Cpf_Cnpj VARCHAR(255) NOT NULL,
-Email VARCHAR(255) NOT NULL UNIQUE,
-Genero CHAR(100),
-Telefone VARCHAR(100),
-Senha VARCHAR(20),
-TipoUsuarioId BINARY(20),
-EnderecoID BINARY(20),
-DataCriacao DATETIME,
-DataModificacao DATETIME,
-PRIMARY KEY (Id),
-FOREIGN KEY (TipoUsuarioId) REFERENCES tipoUsuario(Id),
-FOREIGN KEY (EnderecoId) REFERENCES endereco(Id)
+id BINARY(16) NOT NULL,
+nome VARCHAR(255) NOT NULL,
+cpf_Cnpj VARCHAR(255) NOT NULL,
+email VARCHAR(255) NOT NULL UNIQUE,
+genero CHAR(100),
+telefone VARCHAR(100),
+senha VARCHAR(20),
+tipo_Usuario_id BINARY(20),
+endereco_id BINARY(20),
+tipo_usuario TINYINT,
+PRIMARY KEY (id),
+-- FOREIGN KEY (tipo_Usuario_id) REFERENCES tipo_usuario(id), 
+FOREIGN KEY (endereco_id) REFERENCES endereco(id)
 );
 
 CREATE TABLE anuncio(
-Id BINARY (16) NOT NULL,
-Titulo VARCHAR (255) NOT NULL,
-Url_imagem VARCHAR (255) NOT NULL,
-UsuarioId BINARY (16) NOT NULL,
-TipoStatusId BINARY (16) NOT NULL,
-Disponibilidade VARCHAR (255) NOT NULL,
-Periodo VARCHAR (255) NOT NULL, 
-DataCriacao DATETIME,
-DataModificacao DATETIME,
-PRIMARY KEY (Id),
-FOREIGN KEY (UsuarioId) REFERENCES usuario (Id),
-FOREIGN KEY (TipoStatusId) REFERENCES tipoStatus (Id)
+id BINARY (16) NOT NULL,
+titulo VARCHAR (255) NOT NULL,
+url_imagem VARCHAR (255) NOT NULL,
+usuario_id BINARY (16) NOT NULL,
+tipo_status_id BINARY (16) NOT NULL,
+disponibilidade VARCHAR (255) NOT NULL,
+periodo VARCHAR (255) NOT NULL, 
+PRIMARY KEY (id),
+FOREIGN KEY (usuario_id) REFERENCES usuario(id),
+FOREIGN KEY (tipo_status_id) REFERENCES tipo_status (id)
 );
 
 CREATE TABLE coleta (
-Id BINARY (16) NOT NULL,
-Disponibilidade TEXT (255),
-AnuncioId BINARY (16) NOT NULL, 
-UsuarioId BINARY (16) NOT NULL,
-TipoStatusId BINARY (16) NOT NULL,
-DataCriacao DATETIME,
-DataModificacao DATETIME,
-PRIMARY KEY (Id),
-FOREIGN KEY (AnuncioId) REFERENCES anuncio (Id),
-FOREIGN KEY (UsuarioId) REFERENCES usuario (Id),
-FOREIGN KEY (TipoStatusId) REFERENCES tipostatus (Id)
+id BINARY (16) NOT NULL,
+disponibilidade TEXT (255),
+anuncio_id BINARY (16) NOT NULL, 
+usuario_id BINARY (16) NOT NULL,
+tipo_status_id BINARY (16) NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (anuncio_id) REFERENCES anuncio (id),
+FOREIGN KEY (usuario_id) REFERENCES usuario (id),
+FOREIGN KEY (tipo_status_id) REFERENCES tipo_status (id)
 );
 
 CREATE TABLE produto (
-Id BINARY (16) NOT NULL,
-Nome VARCHAR (255) NOT NULL,
-Quantidade VARCHAR (255) NOT NULL,
-AnuncioId BINARY (16) NOT NULL,
-CategoriaId BINARY (16) NOT NULL,
-DataCriacao DATETIME,
-DataModificacao DATETIME,
-PRIMARY KEY (Id),
-FOREIGN KEY (AnuncioId) REFERENCES anuncio (Id),
-FOREIGN KEY (CategoriaId) REFERENCES categoria (Id)
+id BINARY (16) NOT NULL,
+nome VARCHAR (255) NOT NULL,
+quantidade VARCHAR (255) NOT NULL,
+anuncio_id BINARY (16) NOT NULL,
+categoria_id BINARY (16) NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (anuncio_id) REFERENCES anuncio (id),
+FOREIGN KEY (categoria_id) REFERENCES categoria (id)
 );
 
 
-INSERT INTO tipoStatus 
+INSERT INTO tipo_status 
 VALUES ( (UUID_TO_BIN(UUID())), 
 "AGUARDANDO COLETA"),
 ( (UUID_TO_BIN(UUID())), 
@@ -105,13 +96,14 @@ VALUES ( (UUID_TO_BIN(UUID())),
 ( (UUID_TO_BIN(UUID())), 
 "COLETA FINALIZADA");
 
-INSERT INTO tipoUsuario
-VALUES ( (UUID_TO_BIN(UUID())), 
-"ADMIN"),
-( (UUID_TO_BIN(UUID())), 
-"DOADOR"),
-( (UUID_TO_BIN(UUID())), 
-"COLETOR");
+-- Desativado para testes e validação 
+-- INSERT INTO tipo_usuario
+-- VALUES ( (UUID_TO_BIN(UUID())), 
+-- "ADMIN"),
+-- ( (UUID_TO_BIN(UUID())), 
+-- "DOADOR"),
+-- ( (UUID_TO_BIN(UUID())), 
+-- "COLETOR");
 
 INSERT INTO categoria
 VALUES ( (UUID_TO_BIN(UUID())), 
